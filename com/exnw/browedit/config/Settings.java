ec93e87d3009137a7ac9f4459831bf4f947fd24c
Johan Talboom <borfje@gmail.com>
2012-08-27 11:13:14 +0000

    Added support for generenic and specific configfiles per hostname / username / generic

--- com/exnw/browedit/config/Settings.java
+++ com/exnw/browedit/config/Settings.java
@@ -1,23 +1,47 @@
 package com.exnw.browedit.config;
 
+import java.io.FileNotFoundException;
+import java.net.UnknownHostException;
+
 public class Settings{
 	public static org.json.JSONObject json;
 	
 	static{
-		try{
-			Settings.setConfigurationFile( "settings."+System.getProperty("user.name")+".json" );
-		}catch( java.io.FileNotFoundException e ){
-			System.err.println( "Configuration file \"settings.json\" was not found." );
-			e.printStackTrace();
-			System.exit( 1 );
-		}catch( org.json.JSONException e ){
-			System.err.println( "Invalid configuration in \"settings.json\"." );
-			e.printStackTrace();
+		Settings.json = null;
+		String hostname = "";
+		try
+		{
+			hostname = java.net.InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e1)
+		{
+			e1.printStackTrace();
+		}
+		setConfig("settings." + hostname + "." + System.getProperty("user.name")+".json");
+
+		if(Settings.json == null)
+			setConfig("settings."+System.getProperty("user.name")+".json");
+		if(Settings.json == null)
+			setConfig("settings.json");
+		if(Settings.json == null)
+		{
+			new FileNotFoundException("settings.json").printStackTrace();
 			System.exit( 1 );
 		}
+
 	}
 	
 	public static void setConfigurationFile( String file ) throws java.io.FileNotFoundException, org.json.JSONException{
 		Settings.json = new org.json.JSONObject( new org.json.JSONTokener( new java.io.FileInputStream( file ) ) );
+	}
+
+	private static void setConfig(String fileName)
+	{
+		try{
+			Settings.setConfigurationFile(fileName);
+		}catch( java.io.FileNotFoundException e ){
+			System.err.println( "Configuration file \""+fileName+"\" was not found." );
+		}catch( org.json.JSONException e ){
+			System.err.println( "Invalid configuration in \""+fileName+"\"." );
+		}
 	}
 }
