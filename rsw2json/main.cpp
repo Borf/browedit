069201f38a6165effb529bf77563b063381c4fbc
Johan Talboom <borfje@gmail.com>
2014-07-09 07:02:14 +0000

    *ahem* added the rsw2json source files too

---
+++ rsw2json/main.cpp
@@ -0,0 +1,132 @@
+#include <BroLib/Rsw.h>
+#include <fstream>
+#include <json/json.h>
+#include <json/writer.h>
+#include <string>
+
+#include <blib/util/FileSystem.h>
+
+
+Json::Value vec3(const glm::vec3 &val)
+{
+	Json::Value ret;
+	ret[0u] = val.x;
+	ret[1u] = val.y;
+	ret[2u] = val.z;
+	return ret;
+}
+
+int main(int argc, char* argv[])
+{
+	std::string filename = argv[1];
+	filename = filename.substr(0, filename.rfind("."));
+
+
+	blib::util::FileSystem::registerHandler(new blib::util::PhysicalFileSystemHandler(""));
+
+	printf("Opening %s\n", filename.c_str());
+
+	Rsw* rsw = new Rsw(filename);
+
+	Json::Value data;
+
+	data["version"] = rsw->version;
+	data["inifile"] = rsw->iniFile;
+	data["gndfile"] = rsw->gndFile;
+	data["gatfile"] = rsw->gatFile;
+
+	data["water"]["height"] = rsw->water.height;
+	data["water"]["type"] = rsw->water.type;
+	data["water"]["amplitude"] = rsw->water.amplitude;
+	data["water"]["phase"] = rsw->water.phase;
+	data["water"]["surfaceCurve"] = rsw->water.surfaceCurve;
+	data["water"]["animSpeed"] = rsw->water.animSpeed;
+
+	data["light"]["longitude"] = rsw->light.longitude;
+	data["light"]["lattitude"] = rsw->light.lattitude;
+	data["light"]["diffuse"] = vec3(rsw->light.diffuse);
+	data["light"]["ambient"]= vec3(rsw->light.ambient);
+	data["light"]["intensity"] = rsw->light.intensity;
+
+	for (int i = 0; i < 4; i++)
+		data["unknown"][i] = rsw->unknown[i];
+
+
+	for (int i = 0; i < rsw->objects.size(); i++)
+	{
+		Json::Value o;
+
+		Rsw::Object* object = rsw->objects[i];
+		switch (object->type)
+		{
+		case Rsw::Object::Type::Model: //1
+		{
+			o["type"] = "model";
+			Rsw::Model* model = (Rsw::Model*)object;
+
+			o["name"] = model->name;
+			o["animType"] = model->animType;
+			o["animSpeed"] = model->animSpeed;
+			o["blockType"] = model->blockType;
+			o["fileName"] = model->fileName;
+			//unknown!!!!
+			o["position"] = vec3(model->position);
+			o["rotation"] = vec3(model->rotation);
+			o["scale"] = vec3(model->scale);
+		}
+			break;
+		case Rsw::Object::Type::Light:	//2 Light
+		{
+			o["type"] = "light";
+			Rsw::Light* light = (Rsw::Light*)object;
+			o["name"] = light->name;
+			o["position"] = vec3(light->position);
+			o["todo"] = light->todo; 
+			o["color"] = vec3(light->color);
+			o["todo2"] = light->todo2;
+		}
+			break;
+		case Rsw::Object::Type::Sound://3: //Sound
+		{
+			o["type"] = "sound";
+			Rsw::Sound* sound = new Rsw::Sound();
+			o["name"] = sound->name; 
+			o["filename"] = sound->fileName;
+			o["position"] = vec3(sound->position);
+			o["volume"] = sound->vol;
+			o["width"] = sound->width;
+			o["height"] = sound->height;
+			o["range"] = sound->range;
+//			if (rsw->version >= 0x0200)
+//				pFile->writeFloat(0); //cycle
+		}
+			break;
+		case Rsw::Object::Type::Effect://4: //Effect
+		{
+			o["type"] = "effect";
+			Rsw::Effect* effect = (Rsw::Effect*)object;
+			o["name"] = effect->name;
+			o["position"] = vec3(effect->position);
+			o["id"] = effect->id; 
+			o["loop"] = effect->loop;
+			o["param1"] = effect->param1;
+			o["param2"] = effect->param2;
+			o["param3"] = effect->param3;
+			o["param4"] = effect->param4;
+		}
+			break;
+		}
+		data["objects"].append(o);
+	}
+
+
+	Json::StyledStreamWriter writer;
+	std::ofstream file((filename + ".json").c_str(), std::ios_base::out | std::ios_base::binary);
+	writer.write(file, data);
+	file.close();
+
+	printf("Wrote to %s\n", (filename + ".json").c_str());
+
+	getchar();
+	return 0;
+}
